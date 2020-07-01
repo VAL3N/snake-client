b76b969028e7df7669ff566c68cb021267df113e
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-01 19:56:31 +0000

    refactored code

---
+++ input.js
@@ -0,0 +1,16 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = (data) => {
+  const stdin = process.stdin;
+  if (data === '\u0003') {
+    process.exit();
+  }
+}
+
+module.exports = setupInput;
