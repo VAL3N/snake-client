acfefdcd307dcbf715468c4755dea0f006b0bf04
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-01 19:49:56 +0000

    modified

--- client.js
+++ client.js
@@ -2,10 +2,32 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
+    host: '135.23.222.148',
+    port: 50541,
   });
   // interpret incoming data as text
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!')
+    conn.write("Name: FFG");
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!')
+    setInterval(() => {
+      conn.write("Move: up");
+      conn.write("Move: left");
+      conn.write("Move: down");
+      conn.write("Move: right");
+    }, 500);
+  });
+
+  // process.stdin.on('data', (typedData) => {
+  //   conn.write("Move: up");
+  //   conn.write("Move: down");
+  //   conn.write("Move: left");
+  //   conn.write("Move: right");
+  // });
+
   conn.setEncoding('utf8');
   conn.on('data', (data) => {
     console.log('Message from client: ', data)
