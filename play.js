1dbddcb1558e2bc1335e527f0b7b2aea48a906a1
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-01 20:38:15 +0000

    modified

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
-const { connect } = require('./client');
-const { setupInput } = require('./input');
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 console.log('Connecting ...');
-connect();
-setupInput();
-
-
-// const setupInput = function() {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding('utf8');
-//   stdin.resume();
-//   return stdin;
-// }
-
-// const handleUserInput = (data) => {
-//   const stdin = process.stdin;
-//   if (data === '\u0003') {
-//     process.exit();
-//   }
-// }
+let conn = connect();
+setupInput(conn);
