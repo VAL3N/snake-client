12490f04a6ddb7b1fd6891ff299c4bcdc09b51d4
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-01 14:50:51 +0000

    modified

--- play.js
+++ play.js
@@ -2,19 +2,3 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
-
-/**
- * Establishes connection with the game server
- */
-// const connect = function() {
-//   const conn = net.createConnection({ 
-//     host: 'localhost',
-//     port: 50541
-//   });
-//   // interpret incoming data as text
-//   conn.setEncoding('utf8');
-//   conn.on('data', (data) => {
-//     console.log('Message from client: ', data)
-//   });
-//   return conn;
-// }
