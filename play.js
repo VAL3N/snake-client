a835af349e8d2f7a642f79534966e0152224b86a
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-01 14:49:14 +0000

    refactored code

--- play.js
+++ play.js
@@ -1,20 +1,20 @@
-const net = require('net');
+const connect = require('./client');
+
+console.log('Connecting ...');
+connect();
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log('Message from client: ', data)
-  });
-  return conn;
-}
-
-console.log('Connecting ...');
-connect();
+// const connect = function() {
+//   const conn = net.createConnection({ 
+//     host: 'localhost',
+//     port: 50541
+//   });
+//   // interpret incoming data as text
+//   conn.setEncoding('utf8');
+//   conn.on('data', (data) => {
+//     console.log('Message from client: ', data)
+//   });
+//   return conn;
+// }
